9c3a1e0451a42bc10da0dc8dbede42eee043c265
Tan Jun Yu <tjun-yu@u91z05s05.42kl.edu.my>
2024-03-12 13:05:20 +0800

    Understand how 'pipe' and 'dup2' works

--- dup2.c
+++ dup2.c
@@ -2,25 +2,25 @@
 # include <unistd.h>
 # include <fcntl.h>
 
-#include <stdio.h>
-
 int main(void)
 {
-	FILE *file = fopen("test.txt", "r");
-	FILE *file1 = fopen("test1.txt", "w");
-	char c;
+	int file = open("test.txt", O_RDONLY);
+	int file1 = open("test1.txt", O_WRONLY);
+	int fd[2];
 
-	while ((c = fgetc(file)) != EOF)
-		printf("%c", c);
+	pipe(fd);
 
-	printf("\n----------------\n");
+	// Reading
+	dup2(file, fd[1]);
+	off_t file_size = lseek(file, 0, SEEK_END) - lseek(file, 0, SEEK_SET) + 1;
+	char buffer[file_size];
+	read(fd[0], buffer, file_size);
+	buffer[file_size] = '\0';
 
-	rewind(file);
-	dup2(fileno(file1), fileno(stdout)); // Redirect stdout to file1
-	while ((c = fgetc(file)) != EOF)
-		printf("%c", c);
+	// Writing
+	dup2(file1, fd[0]);
+	write(fd[1], buffer, file_size);
 
-	fclose(file);
-	fclose(file1);
-	return 0;
+	close(file);
+	close(file1);
 }
--- pipe.c
+++ pipe.c
@@ -1,17 +1,24 @@
-# include <stdio.h>
-# include <unistd.h>
-# include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
-int main(void)
-{
-	int file = open("test1.txt", O_WRONLY);
-	int fd[2];
+int main() {
+    int fd[2];
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        return 1;
+    }
 
-	pipe(fd);
+    printf("Read file descriptor: %d\n", fd[0]);
+    printf("Write file descriptor: %d\n", fd[1]);
 
-	// Writing
-	dup2(file, fd[1]);
-	write(fd[1], "hello world", 11);
+    // You can now use fd[0] to read from the pipe and fd[1] to write to the pipe.
+	write(fd[1], "hello world\n", 12);
+	char buffer[12];
+	read(fd[0], buffer, 12);
+	printf("Read from pipe: %s", buffer);
 
-	// Reading
+    close(fd[0]);
+    close(fd[1]);
+
+    return 0;
 }
