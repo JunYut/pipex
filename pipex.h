297d4ce2da5d5ad02db245066d708c343e1f5d31
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-03-18 15:13:07 +0800

    pipex.h: included <string.h> & <errno.h>

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 11:19:38 by we                #+#    #+#             */
-/*   Updated: 2024/03/18 12:24:54 by we               ###   ########.fr       */
+/*   Updated: 2024/03/18 15:11:51 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 #include <stdlib.h>
 #include "libft/c_libft.h"
 
