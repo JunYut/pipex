a8f3198bd774b51e36ee713691deebd2d64d89b8
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-03-18 12:47:00 +0800

    pipex.h: created header file

---
+++ pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/17 11:19:38 by we                #+#    #+#             */
+/*   Updated: 2024/03/18 12:24:54 by we               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "libft/c_libft.h"
+
+void	validation(int argc, char *argv[]);
+
+#endif
