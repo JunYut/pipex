0399a23e9597868d242ef8de765aad36ba4059b4
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-03-21 11:01:53 +0800

    src/utils.c: now frees 't_pipex' after freeing the strings

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 16:53:18 by we                #+#    #+#             */
-/*   Updated: 2024/03/21 10:04:05 by we               ###   ########.fr       */
+/*   Updated: 2024/03/21 10:54:13 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	clean_up(t_pipex *var)
 	free_words(var->cmd2);
 	free_words(var->path1);
 	free_words(var->path2);
+	free(var);
+	var = NULL;
 }
 
 void	free_words(char *words[])
