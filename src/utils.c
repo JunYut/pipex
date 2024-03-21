8f41804f30a44cb0b6e55ee6501d75116afdac58
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-03-21 11:10:45 +0800

    src/utils.c: implemented 'c_malloc' to handle memory allocation failures

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 16:53:18 by we                #+#    #+#             */
-/*   Updated: 2024/03/21 10:54:13 by we               ###   ########.fr       */
+/*   Updated: 2024/03/21 11:08:10 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 void	clean_up(t_pipex *var)
 {
+	printf("debug6\n");	// debug
 	free_words(var->cmd1);
+	printf("debug7\n");	// debug
 	free_words(var->cmd2);
+	printf("debug8\n");	// debug
 	free_words(var->path1);
+	printf("debug9\n");	// debug
 	free_words(var->path2);
+	printf("debug10\n");	// debug
 	free(var);
+	printf("debug11\n");	// debug
 	var = NULL;
 }
 
@@ -34,4 +40,17 @@ void	free_words(char *words[])
 		}
 		free(words);
 	}
+}
+
+void	*c_malloc(size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (ptr == NULL)
+	{
+		perror("Error: malloc");
+		exit(1);
+	}
+	return (ptr);
 }
